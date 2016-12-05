c19f82b560f036a0b7de5661f128e18206717eb2
Jwata <watanabe0621@gmail.com>
2016-12-05 23:37:06 +0900

    first wai app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+
 module Main where
 
-import Lib
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
+import Data.ByteString.Char8 as C
+import Data.ByteString.Lazy.Char8 as LC
+
+app :: Application
+app req respond = respond
+  $ responseLBS status200 [(hContentType, C.pack "text/plain")] (LC.pack "Hello World!")
 
 main :: IO ()
-main = someFunc
+main = run 8080 app
+
