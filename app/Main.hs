168acb33ca199f5e4a9c96ebc72cf3a6a42e2fec
Jwata <watanabe0621@gmail.com>
2016-12-08 17:58:38 +0900

    return html file

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
-{-# OPTIONS_GHC -fno-warn-deprecations #-}
-
 module Main where
 
-import Network.Wai (Application, responseLBS)
+import Network.Wai (Application, responseLBS, responseFile)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
@@ -10,9 +8,11 @@ import Data.ByteString.Char8 as C
 import Data.ByteString.Lazy.Char8 as LC
 
 app :: Application
-app req respond = respond
-  $ responseLBS status200 [(hContentType, C.pack "text/plain")] (LC.pack "Hello World!")
+app request respond = respond
+  $ responseFile status200 [(hContentType, C.pack "text/html")] "public/index.html" Nothing
+--   $ responseLBS status200 [(hContentType, C.pack "text/text")] (LC.pack "Hello World!")
 
 main :: IO ()
-main = run 8080 app
-
+main = do
+  print "starting server..."
+  run 8080 app
